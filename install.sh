01508e150fad5f7e4206cf770102c54ad33bad2c
Tom Rees <zephod@gmail.com>
2012-07-11 00:19:36 +0100

    Initial commit. This is my filter & install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FILTER=$DIR/filter
+
+stat $FILTER >/dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    # Filter script found
+    sudo ln -s $FILTER /usr/bin/filter
+else
+    echo "Error: Could not locate script: $FILTER"
+    exit 1
+fi
